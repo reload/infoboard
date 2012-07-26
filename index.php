8370f717178312709522b5cbba9384faa9074063
Rasmus Luckow-Nielsen <rasmus.luckow@gmail.com>
2012-07-26 15:18:36 +0200

    First version

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+
+    <title>Reload infoboard rotator</title>
+    <meta name="description" content="">
+
+    <meta name="viewport" content="width=device-width">
+    <link rel="shortcut icon" href="http://reload.dk/favicon.ico">
+
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/vendor/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+    <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
+         chromium.org/developers/how-tos/chrome-frame-getting-started -->
+    <!--[if lt IE 7]><p class="chromeframe">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+  <figure id="progress"></figure>
+  
+  <iframe id="viewer" src="http://google.dk" scrolling="no" height="100%" width="100%"></iframe>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+  <!-- end scripts -->    
+    
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,75 @@
+var urls = {
+  0: [
+    {"url": "http://timeking.reload.dk"}
+  ],
+  1: [
+    {"url": "https://reload.geckoboard.com/dashboard/470D0EB4CA98E67D/"}
+  ],
+  2: [
+    {"url": "http://visibletweets.com/#query=%23drupal%20OR%20%40reloaddk%20lang%3Ada&animation=2"}
+  ]
+};
+
+var items = 3;
+var pointer = 0;
+var delay = 20000;
+
+function changeUrl() { 
+  
+  url = urls[pointer][0].url;
+  $('iframe#viewer').attr('src', url);
+
+	// animate the progress bar
+	$('#progress').stop(true,true);
+	$('#progress').css('width','0px');
+	//$('#progress').delay(1).animate({width: '0%'}, 99, 'swing'); 
+  $('#progress').animate({width: '100%'}, delay, 'easeInSine');  
+  
+  pointer++;
+  if(pointer >= items) {
+    pointer = 0;
+  }
+  
+  setTimeout(function(){
+    changeUrl();
+  }, delay);  
+  
+}
+
+
+$(function(){
+
+  changeUrl();
+
+/*  
+  $(document).ready(function(){
+      $('iframe#viewer').attr('src', current_url);    
+  });
+*/
+/*
+  for(var i = 0; i < urls.length; i++){
+  
+    // Goalies gets the moustache 
+    if(data.ranking[i].group == "B-goalie") _item.find('.user_avatar').prepend($('<figure class="sir"></figure>'));
+  
+    // sets the ranking numbers and name
+    //_item.find('.rank').addClass('bg'+i).html(i+1);
+    _item.find('.rank').addClass(data.ranking[i].group).html(groups[data.ranking[i].group][0].icon);
+    _item.find('h2').html(data.ranking[i].name+'.');
+  
+    // winner and loser get a custom text, everybody else the default text
+    if(data.ranking[i].group == "B-goalie") _item.find('.hours').html('Like a boss!');
+  //		  else if(i == data.ranking.length-1) _item.find('.hours').html('Only <span>'+data.ranking[i].hours_registered+' hours logged</span>. What a whimp!');
+  //		  else _item.find('.hours').html('<span>'+data.ranking[i].hours_registered+' hours logged</span>.');
+  
+    // get a 'funny' sentence from the lines object
+    _item.find('.desc').html(groups[data.ranking[i].group][0].text);
+
+    // append it
+    $('#user_ranking ul').append(_item);
+  
+  }
+*/  
+  
+  
+});
