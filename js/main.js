f885f92ba774d9bad88aaed2bdad569da7bd1078
Rasmus Luckow-Nielsen <rasmus.luckow@gmail.com>
2012-07-27 11:00:21 +0200

    Adjusted height of progress-bar.

--- js/main.js
+++ js/main.js
@@ -36,40 +36,6 @@ function changeUrl() {
   
 }
 
-
 $(function(){
-
   changeUrl();
-
-/*  
-  $(document).ready(function(){
-      $('iframe#viewer').attr('src', current_url);    
-  });
-*/
-/*
-  for(var i = 0; i < urls.length; i++){
-  
-    // Goalies gets the moustache 
-    if(data.ranking[i].group == "B-goalie") _item.find('.user_avatar').prepend($('<figure class="sir"></figure>'));
-  
-    // sets the ranking numbers and name
-    //_item.find('.rank').addClass('bg'+i).html(i+1);
-    _item.find('.rank').addClass(data.ranking[i].group).html(groups[data.ranking[i].group][0].icon);
-    _item.find('h2').html(data.ranking[i].name+'.');
-  
-    // winner and loser get a custom text, everybody else the default text
-    if(data.ranking[i].group == "B-goalie") _item.find('.hours').html('Like a boss!');
-  //		  else if(i == data.ranking.length-1) _item.find('.hours').html('Only <span>'+data.ranking[i].hours_registered+' hours logged</span>. What a whimp!');
-  //		  else _item.find('.hours').html('<span>'+data.ranking[i].hours_registered+' hours logged</span>.');
-  
-    // get a 'funny' sentence from the lines object
-    _item.find('.desc').html(groups[data.ranking[i].group][0].text);
-
-    // append it
-    $('#user_ranking ul').append(_item);
-  
-  }
-*/  
-  
-  
 });
