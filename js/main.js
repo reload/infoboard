f0e9fcda6eed2e39d8ab4da19dbb2f11730527a3
Rasmus Luckow-Nielsen <rasmus.luckow@gmail.com>
2013-01-14 11:54:06 +0100

    Rearranged order

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@ var urls = {
     {"url": "http://timeking.reload.dk"}
   ],
   1: [
-    {"url": "https://reload.geckoboard.com/dashboard/470D0EB4CA98E67D/"}
+    {"url":"http://jenkins.reload.dk/plugin/jenkinswalldisplay/walldisplay.html?viewName=All&jenkinsUrl=http%3A%2F%2Fjenkins.reload.dk%2F"}
   ],
   2: [
-    {"url": "http://visibletweets.com/#query=%23drupal%20OR%20%40reloaddk%20lang%3Ada&animation=2"}
+    {"url": "https://reload.geckoboard.com/dashboard/470D0EB4CA98E67D/"}
   ],
   3: [
-    {"url": "http://reload.dk/opensource#activity"}
+    {"url": "http://visibletweets.com/#query=%23drupal%20OR%20%40reloaddk%20lang%3Ada&animation=2"}
   ],
   4: [
-    {"url": "https://reload.geckoboard.com/dashboard/87CCED14EB760EEE/"}
+    {"url": "http://reload.dk/opensource#activity"}
   ],
   5: [
-    {"url": "http://visibletweets.com/#query=from%3Arasmusluckow%20OR%20from%3Akasperg%20OR%20from%3Axendk%20OR%20from%3Aramlev%20OR%20from%3Atwomasc%20OR%20from%3Ahalfgirl%20OR%20from%3Aarnested%20OR%20from%3Adanquah%20OR%20from%3Atrogels%20OR%20from%3Amartinelvar%20OR%20from%3Ahamderhallas%20OR%20from%3Apaludanent%20OR%20from%3Areloaddk&animation=1"}
+    {"url": "https://reload.geckoboard.com/dashboard/87CCED14EB760EEE/"}
   ],
   6: [
-    {"url":"http://jenkins.reload.dk/plugin/jenkinswalldisplay/walldisplay.html?viewName=All&jenkinsUrl=http%3A%2F%2Fjenkins.reload.dk%2F"}
+    {"url": "http://visibletweets.com/#query=from%3Arasmusluckow%20OR%20from%3Akasperg%20OR%20from%3Axendk%20OR%20from%3Aramlev%20OR%20from%3Atwomasc%20OR%20from%3Ahalfgirl%20OR%20from%3Aarnested%20OR%20from%3Adanquah%20OR%20from%3Atrogels%20OR%20from%3Amartinelvar%20OR%20from%3Ahamderhallas%20OR%20from%3Apaludanent%20OR%20from%3Areloaddk&animation=1"}
   ]
 };
 
