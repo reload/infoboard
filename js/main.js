b9de8afba992bb508f8fdd4a8baa0d6e8c2887df
Rasmus Luckow-Nielsen <luckow@lygoff.local>
2013-10-24 16:18:11 +0200

    Removed Twitter-fountain and site-status page

--- js/main.js
+++ js/main.js
@@ -9,13 +9,7 @@ var urls = {
     {"url": "https://reload.geckoboard.com/dashboard/470D0EB4CA98E67D/"}
   ],
   3: [
-    {"url": "http://twitterfontana.com/fountain.html?embed=true&twitter_search=%40reloaddk+OR+%23drupal+lang%3Ada&effect=Slide&message_animate_interval=6000&font_face=Helvetica%2C+'Helvetica+Neue'%2C+Arial%2C+sans-serif&text_color=%233f3f3f&special_color=%23EFA422&bg_color=%2398be31&box_bg=%23f9f8f7&utm_source=site&utm_medium=embed&utm_campaign=twitterfontana"}
-  ],
-  4: [
     {"url": "http://reload.dk/opensource#activity"}
-  ],
-  5: [
-    {"url": "https://reload.geckoboard.com/dashboard/87CCED14EB760EEE/"}
   ]
 };
 
